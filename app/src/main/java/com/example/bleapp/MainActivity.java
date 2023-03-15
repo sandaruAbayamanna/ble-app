9f2599ed212e96ba240edc9ca007df44b929113b
sandaru <sandaruabayamanna@gmail.com>
2023-03-15 13:17:21 +0530

    bluetooth on/off err fixed

--- app/src/main/java/com/example/bleapp/MainActivity.java
+++ app/src/main/java/com/example/bleapp/MainActivity.java
@@ -8,11 +8,13 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -27,8 +29,8 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 0;
-    private static final int REQUEST_DISCOVER_BT = 1;
-    public BluetoothAdapter bluetoothAdapter;
+    //private static final int REQUEST_DISCOVER_BT = 1;
+
   //  public BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
 
 
@@ -95,14 +97,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //adapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+            BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+
+
+
+
+
 
         BluStatus = findViewById(R.id.bluStatus);
         DeviceList = findViewById(R.id.listDv);
@@ -129,23 +139,13 @@ public class MainActivity extends AppCompatActivity {
         onBtn.setOnClickListener(new View.OnClickListener() {
 
             //@SuppressLint("MissingPermission")
+            @SuppressLint("MissingPermission")
             @Override
             public void onClick(View view) {
                 if (!bluetoothAdapter.isEnabled()) {
                     Toast.makeText(MainActivity.this, "Turning On Bluetooth", Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
                     startActivityForResult(intent, REQUEST_ENABLE_BT);
                 } else {
                     Toast.makeText(MainActivity.this, "Bluetooth Is Already ON", Toast.LENGTH_SHORT).show();
@@ -155,19 +155,11 @@ public class MainActivity extends AppCompatActivity {
 
         //off button
         offBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onClick(View view) {
                 if (bluetoothAdapter.isEnabled()) {
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
+
                     bluetoothAdapter.disable();
                     Toast.makeText(MainActivity.this, "Turning Bluetooth OFF.....", Toast.LENGTH_SHORT).show();
                 } else {
