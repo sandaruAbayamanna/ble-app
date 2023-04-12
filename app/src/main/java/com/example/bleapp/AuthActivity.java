7ffee8671476e7fa08e13822535683ef2c00e991
sandaru <sandaruabayamanna@gmail.com>
2023-04-12 17:18:06 +0530

    config gattt

--- app/src/main/java/com/example/bleapp/AuthActivity.java
+++ app/src/main/java/com/example/bleapp/AuthActivity.java
@@ -3,10 +3,15 @@ package com.example.bleapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -16,6 +21,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.UUID;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -35,8 +42,10 @@ public class AuthActivity extends AppCompatActivity {
         // Get references to the EditText and Button
         mPasswordEdit = findViewById(R.id.password_edit);
         Button mAuthButton = findViewById(R.id.auth_button);
-        // Get the device address from the Intent
+
+        // Get the device address & Name from the Intent
         String deviceAddress = getIntent().getStringExtra(DEVICE_ADDRESS);
+        String deviceName = getIntent().getStringExtra(DEVICE_NAME);
 
         // Get the BluetoothDevice for the selected address
         BluetoothManager bluetoothManager =
@@ -44,37 +53,28 @@ public class AuthActivity extends AppCompatActivity {
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
         mDevice = bluetoothAdapter.getRemoteDevice(deviceAddress);
 
-        /*// Set the device name in the TextView
+        // Set the device name in the TextView
         TextView deviceNameText = findViewById(R.id.device_name_text);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        if (mDevice.getName() != null) {
-            deviceNameText.setText(mDevice.getName());
+
+       /* if (mDevice != null) {
+            deviceNameText.setText(deviceName);
         }
         else {
-            deviceNameText.setText(R.string.unkDev);}
+            deviceNameText.setText(R.string.unkDev);}*/
 
-        deviceNameText.setText(mDevice.getName());*/
+        deviceNameText.setText(deviceAddress);
 
         // Set a click listener on the Button
         mAuthButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Log.i("authButton","login btn clicked");
+                Log.i("authButton", "login btn clicked");
                 // Get the password from the EditText
                 String password = mPasswordEdit.getText().toString();
 
                 Utils.toast(getApplicationContext(), "Login Button Pressed");
-                Log.i("authButton","login btn clicked");
+                Log.i("authButton", "login btn clicked");
 
                 // Start the authentication process
                 authenticate(password);
@@ -82,8 +82,79 @@ public class AuthActivity extends AppCompatActivity {
         });
     }
 
-    private void authenticate(String password){
-        Log.i("auth","authentication started");
+    @SuppressLint("MissingPermission")
+    private void authenticate(String password) {
+        Log.i("auth", "authentication started");
+
+        // Connect to the device
+        mGatt = mDevice.connectGatt(this, false, new BluetoothGattCallback() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                super.onConnectionStateChange(gatt, status, newState);
+
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    // Discover services on the device
+                    //when discoverServices is completed onServicesDiscovered method is called
+                    gatt.discoverServices();
+                    Log.i("connectGatt","discovering services");
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    // Close the GATT connection
+                    gatt.close();
+                }
+
+            }
+
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                super.onServicesDiscovered(gatt, status);
+
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    // Get the service and characteristic for the authentication process
+                    BluetoothGattService authService = gatt.getService(UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb"));
+                    BluetoothGattCharacteristic authChar = authService.getCharacteristic(UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb"));
+
+                    // Authenticate with the device by writing the password to the characteristic
+                    authChar.setValue(password.getBytes());
+                    gatt.writeCharacteristic(authChar);
+                } else {
+                    // Close the GATT connection
+                    gatt.close();
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onCharacteristicWrite(gatt, characteristic, status);
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+
+                    Log.i("GATT","Authentication Success");
+
+
+                    // Authentication was successful, close the GATT connection
+                    gatt.close();
+                } else {
+                    // Authentication failed, close the GATT connection
+                    Log.i("GATT","Authentication failed");
+                    gatt.close();
+
+                }
+            }
+        });
+
     }
 
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Close the GATT connection if it's still open
+        if (mGatt != null) {
+
+            mGatt.close();
+        }
+    }
 }
