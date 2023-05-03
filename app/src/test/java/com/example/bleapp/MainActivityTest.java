d23cb5924b2f556a445a46d1ea76434c99642ab7
sandaru <sandaruabayamanna@gmail.com>
2023-05-03 17:15:55 +0530

    test cases impl

---
+++ app/src/test/java/com/example/bleapp/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.example.bleapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testLaunch(){
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
