a11656cf5610b88a25a8585839a5b6f33c18fa00
sandaru <sandaruabayamanna@gmail.com>
2023-05-15 17:01:38 +0530

    added date class

--- app/src/main/java/com/example/bleapp/DetailActivity.java
+++ app/src/main/java/com/example/bleapp/DetailActivity.java
@@ -1,13 +1,17 @@
 package com.example.bleapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -56,13 +60,39 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         if (item.getItemId() == R.id.delete){
-            NoteDatabaseHelper db = new NoteDatabaseHelper(this);
-            Intent intent = getIntent();
-            id = intent.getIntExtra("ID",0);
-            db.deleteNote(id);
-            Toast.makeText(this, "Note Deleted Successfully", Toast.LENGTH_SHORT).show();
-            Intent intent1 = new Intent(DetailActivity.this,ListNotesActivity.class);
-            startActivity(intent1);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Delete Note");
+            builder.setMessage("Are you sure you want to delete this note?");
+
+            //Add positive button
+            builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //delete note method
+                    NoteDatabaseHelper db = new NoteDatabaseHelper(DetailActivity.this);
+                    Intent intent = getIntent();
+                    id = intent.getIntExtra("ID",0);
+                    db.deleteNote(id);
+                    Toast.makeText(DetailActivity.this, "Note Deleted Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent1 = new Intent(DetailActivity.this,ListNotesActivity.class);
+                    startActivity(intent1);
+                }
+            });
+
+            //Add negative button
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return true;
+
+
 
         }
         return super.onOptionsItemSelected(item);
